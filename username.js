3b4b16e5ae1a6c5d34b42e1821f30d487cffc7ac
JacobLeisen <97263734+JacobLeisen@users.noreply.github.com>
2022-01-11 12:25:19 -0600

    Add files via upload

--- username.js
+++ username.js
@@ -1,1 +1,25 @@
-console.log('something idk');
+const adjective = ['mini', 'giant', 'fluffy', 'ugly', 'cozy', 'hairy', 'tasty', 'small', 'amazing', 'monster'];
+const animal = ['hippo', 'cat', 'dog', 'monkey', 'squid', 'giraffe', 'bird', 'eagle', 'spider', 'fox'];
+
+const adjGenerator = arr => {
+    let adj = arr[Math.floor(Math.random() * arr.length)];
+    return adj;
+};
+const animalGenerator = arr => {
+    let animal = arr[Math.floor(Math.random() * arr.length)];
+    return animal;
+};
+const numGenerator = () => {
+    let num = Math.floor(Math.random() * 1000);
+    return num;
+};
+let username = adjGenerator(adjective) + animalGenerator(animal) + numGenerator();
+
+
+console.log(username);
+
+
+
+
+
+
